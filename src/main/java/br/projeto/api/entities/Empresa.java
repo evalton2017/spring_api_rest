a11856c3fd88fe63d063a4bac0b355aee30c119d
evalton <evalton.nunes@gmail.com>
2020-02-08 14:08:09 -0300

    criacao repository empresa e funcionario

--- src/main/java/br/projeto/api/entities/Empresa.java
+++ src/main/java/br/projeto/api/entities/Empresa.java
@@ -38,6 +38,18 @@ public class Empresa implements Serializable{
 	public Empresa() {
 		
 	}
+	
+
+	public Empresa(Long id, String razaoSocial, String cnpj, Date dataCriacao, Date dataAtualizacao) {
+		super();
+		this.id = id;
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.dataCriacao = dataCriacao;
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+
 
 	public Long getId() {
 		return id;
@@ -106,14 +118,15 @@ public class Empresa implements Serializable{
 				+ ", dataAtualizacao=" + dataAtualizacao + "]";
 	}
 
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cnpj == null) ? 0 : cnpj.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -124,11 +137,6 @@ public class Empresa implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Empresa other = (Empresa) obj;
-		if (cnpj == null) {
-			if (other.cnpj != null)
-				return false;
-		} else if (!cnpj.equals(other.cnpj))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -136,6 +144,8 @@ public class Empresa implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
--- src/test/java/br/projeto/api/ApiSpringSwaggerApplicationTests.java
+++ src/test/java/br/projeto/api/ApiSpringSwaggerApplicationTests.java
@@ -1,13 +1,16 @@
 package br.projeto.api;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.projeto.api.entities.Empresa;
+import br.projeto.api.repository.EmpresaRepository;
 
 @SpringBootTest
 class ApiSpringSwaggerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
